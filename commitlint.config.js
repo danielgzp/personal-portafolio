f61241c32a6237a735db92441814ffc5091ba3ae
Daniel Gonzalez <danielgzp01@gmail.com>
2026-07-12 21:16:51 -0400

    chore: extend commitlint config with advanced rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,33 @@
+/** @type {import('@commitlint/types').UserConfig} */
 export default {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-leading-blank": [1, "always"],
+    "footer-leading-blank": [1, "always"],
+    "header-max-length": [2, "always", 120],
+    "scope-case": [2, "always", "kebab-case"],
+    "subject-case": [0],
+    "subject-empty": [2, "never"],
+    "type-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "deps",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "ui",
+        "wip"
+      ]
+    ]
+  }
 };
